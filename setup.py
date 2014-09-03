9ff1ed61228c1d7fc68c6cfa233a6e6d6d0d12be
Jacob.Carley <Jacob.Carley@fe8.zeus.fairmont.rdhpcs.noaa.gov>
2014-09-03 15:41:06 +0000

    Some fixes to grammar.

--- setup.py
+++ setup.py
@@ -2,13 +2,9 @@ from setuptools import setup
 
 setup(
   name              = "ncepy",
-#  install_requires = ["matplotlib",
-#                      "numpy",
-#                      "scipy",
-#                      "mpl_toolkits.basemap"],
   version           = "0.1",
-  description       = "Useful toolkit for analysis and visualization weather, climate, and ocean data.",
-  long_description  = """ This is a toolkit for analysis and visualization weather, climate, and ocean data.  It is not a package which creates graphics or plots, but rather helps augment such packages.  In this toolkit, users will find routines which rotate grid relative winds to earth relative, generate color tables, identify corners for plotting regions, convert units, etc. """,
+  description       = "Useful toolkit for analysis and visualization of weather, climate, and ocean data.",
+  long_description  = """ This is a toolkit for analysis and visualization of weather, climate, and ocean data.  It is not a package which creates graphics or plots, but rather helps augment such packages.  In this toolkit users will find routines which rotate grid relative winds to earth relative, generate color tables, identify corners for plotting regions, convert units, etc. """,
   url               = "https://github.com/jrcarley/NCEPy",
   author            = "Jacob Carley",
   author_email      = "jacob.carley@noaa.gov",
@@ -31,8 +27,4 @@ setup(
 #Development Status :: 5 - Production/Stable
 #Development Status :: 6 - Mature
 #Development Status :: 7 - Inactive
-
-
-
-
 
